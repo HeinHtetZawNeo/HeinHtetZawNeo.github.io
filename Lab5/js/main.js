ebfe8df2ed0314abdb75d91d1f1979ee66820a0f
HeinHtetZawNeo <HeinHtetZawNeo@users.noreply.github.com>
2023-11-03 17:41:20 -0500

    submit lab 5

---
+++ Lab5/js/main.js
@@ -0,0 +1,11 @@
+let exerciseOne = (...input) => input.filter(ele=>ele>20).reduce((accumulator,currentVal)=>accumulator+currentVal,0);
+let getNewArray = (strArray)=>strArray.filter(ele=>ele.length >=5).filter(ele=>ele.includes('a'));
+let exerciseThree = (arg1,arg2,arg3)=>[...arg1,...arg2,...arg3];
+
+
+
+console.log(exerciseOne(2, 3, 4, 5, 3));
+
+let strAry = ["Hello","String 1","abcdefg","baccdefg"];
+console.log(getNewArray(strAry));
+console.log(exerciseThree('hi', [1,2,3], ['Hello','world']));
