1aa2f48061579b294cb2cb1859bd84e6dfcb192f
HeinHtetZawNeo <HeinHtetZawNeo@users.noreply.github.com>
2023-11-06 22:11:46 -0600

    fix bug

--- Lab6/ProblemC.js
+++ Lab6/ProblemC.js
@@ -1,12 +1,14 @@
 function printNumbers(from, to) {
     let timer = setInterval(() => {
-        if (from <= to)
-            document.getElementById("Number").innerHTML= from++;
-        else{
+        if (from <= to) {
+            document.getElementById("Number").classList.remove("blink");
+            document.getElementById("Number").innerHTML = from++;
+
+        } else {
             document.getElementById("Number").classList.add("blink");
             clearTimeout(timer);
         }
-            
+
     }, 1000);
 }
 
