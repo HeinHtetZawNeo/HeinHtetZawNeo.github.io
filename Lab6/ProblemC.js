578b3652535e026ecc9703dde7a560c5e5ccc6c4
HeinHtetZawNeo <HeinHtetZawNeo@users.noreply.github.com>
2023-11-06 22:03:08 -0600

    Lab 6

--- Lab6/ProblemC.js
+++ Lab6/ProblemC.js
@@ -1,11 +1,12 @@
 function printNumbers(from, to) {
     let timer = setInterval(() => {
-        if (from != to)
-            console.log(++from);
-        else
+        if (from <= to)
+            document.getElementById("Number").innerHTML= from++;
+        else{
+            document.getElementById("Number").classList.add("blink");
             clearTimeout(timer);
+        }
+            
     }, 1000);
 }
-
-printNumbers(10, 20);
 
